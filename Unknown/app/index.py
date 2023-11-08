708dddee6265cb257306a798818b0b8ffdc3e937
NHBKhang <109735934+NHBKhang@users.noreply.github.com>
2023-11-08 17:22:31 +0700

    Add files via upload

--- Unknown/app/index.py
+++ Unknown/app/index.py
@@ -1,6 +1,8 @@
-from  flask import Flask, render_template
+from  flask import render_template
 
-app = Flask(__name__)
+from app import app
+
+
 @app.route('/')
 def index():
     return  render_template('index.html')
