f0028f15b53a226d5197965324cb9be8101af2f5
NHBKhang <nhbkhang12@gmail.com>
2023-11-09 19:01:47 +0700

    v

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from urllib.parse import quote
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:%s@localhost/saledb?charset=utf8mb4" % quote("Admin@123")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
+db = SQLAlchemy(app=app)
+
