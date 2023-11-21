fd9800ec8cb090bfb1b30edbb5fb59177dafafd6
NHBKhang <nhbkhang12@gmail.com>
2023-11-21 22:08:04 +0700

    new

--- app/index.py
+++ app/index.py
@@ -5,7 +5,8 @@ from app import app
 
 @app.route('/')
 def index():
-    return  render_template('index.html')
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
